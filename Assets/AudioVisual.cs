77de946c5dcf9474f05dfe5a74628eb34b504b7f
Sam <sammydeedge@hotmail.com>
2018-12-04 16:23:01 +1100

    Created Audio Waveform Generator

---
+++ Assets/AudioVisual.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioVisual : MonoBehaviour {
+
+    private const int SAMPLE_SIZE = 1024;
+
+    public float rmsValue;
+    public float dbValue;
+    public float pitchValue;
+
+    private AudioSource source;
+    private float[] samples;
+    private float[] spectrum;
+    private float sampleRate;
+
+    private Transform[] visualList;
+    private float[] visualScale;
+    private int amnVisual = 24;
+
+
+    public float maxVisualScale = 25.0f;
+    public float scalefactorY = 50.0f;
+    public float smoothSpeed = 10.0f;
+    public float keepPercent = 0.5f;
+
+    // Use this for initialization
+    void Start () {
+        source = GetComponent<AudioSource>();
+        samples = new float[SAMPLE_SIZE];
+        spectrum = new float[SAMPLE_SIZE];
+        sampleRate = AudioSettings.outputSampleRate;
+
+        SpawnLine();
+	}
+
+    private void SpawnLine()
+    {
+        visualScale = new float[amnVisual];
+        visualList = new Transform[amnVisual];
+
+        for (int i = 0; i < amnVisual; i++)
+        {
+            GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube) as GameObject;
+            visualList[i] = go.transform;
+            visualList[i].position = Vector3.right * i;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        AnalyzeSound();
+        UpdateVisuals();
+	}
+
+    void UpdateVisuals()
+    {
+        int visualIndex = 0;
+        int spectrumIndex = 0;
+        int averageSize = (int)(SAMPLE_SIZE * keepPercent) / amnVisual;
+        while (visualIndex < amnVisual)
+        {
+            int j = 0;
+            float sum = 0;
+            while (j < averageSize)
+            {
+                sum += spectrum[spectrumIndex];
+                spectrumIndex++;
+                j++;
+            }
+
+            float scaleY = sum / averageSize * scalefactorY;
+            visualScale[visualIndex] -= Time.deltaTime * smoothSpeed;
+            if(visualScale[visualIndex] < scaleY)
+            {
+                visualScale[visualIndex] = scaleY;
+            }
+
+            if(visualScale[visualIndex] > maxVisualScale)
+            {
+                visualScale[visualIndex] = maxVisualScale;
+            }
+            visualList[visualIndex].localScale = Vector3.one + Vector3.up * visualScale[visualIndex];
+            visualIndex++;
+        }
+    }
+
+    void AnalyzeSound()
+    {
+        source.GetOutputData(samples, 0);
+
+        int i = 0;
+        float sum = 0;
+        for (i = 0; i < SAMPLE_SIZE; i++)
+        {
+            sum += samples[i] * samples[i];
+        }
+        rmsValue = Mathf.Sqrt(sum / SAMPLE_SIZE);
+
+        //db
+        dbValue = 20 * Mathf.Log10(rmsValue / 0.1f);
+
+        //sound spectrum
+        source.GetSpectrumData(spectrum, 0, FFTWindow.BlackmanHarris);
+
+        //pitch (optional)
+
+
+    }
+}
